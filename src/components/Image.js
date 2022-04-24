50041bd5c095d2610205f2eca1155e504679f6f2
Alvaro Andrade <alvaro.andrade.a@mail.pucv.cl>
2022-04-24 14:26:03 -0400

    ✨Filtros ✨

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Image.css";
 class Image extends React.Component {
   constructor(props) {
     super(props);
@@ -7,11 +8,14 @@ class Image extends React.Component {
     var imgStyle = {
       filter: ` contrast(${this.props.settings[0].value}) hue-rotate(${this.props.settings[1].value}) brightness(${this.props.settings[2].value}) saturate(${this.props.settings[3].value}) sepia(${this.props.settings[4].value})
 invert(${this.props.settings[5].value})`,
-      backgroundImage: `url(${this.props.url})`,
     };
     return (
       <div className="imageContainer">
-        <img className="guitar" style={imgStyle} />{" "}
+        <img
+          className="guitar"
+          src="https://www.w3schools.com/w3images/sound.jpg"
+          style={imgStyle}
+        />{" "}
       </div>
     );
   }
