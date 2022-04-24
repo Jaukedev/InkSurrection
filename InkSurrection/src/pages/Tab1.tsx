eda6831d379a3b94f0716b0f897f1d36167da854
Alvaro Andrade <alvaro.andrade.a@mail.pucv.cl>
2022-04-24 13:28:23 -0400

    💩💩 Armando logica anterior💩💩

--- InkSurrection/src/pages/Tab1.tsx
+++ InkSurrection/src/pages/Tab1.tsx
@@ -1,8 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRow, IonCol } from '@ionic/react';
+import { image } from 'ionicons/icons';
+import logo from "../assets/icon/INK.png"
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+// import { NgxDropzoneComponent, NgxDropzonePreviewComponent } from 'ngx-dropzone';
+import './Tab1.scss';
 
 const Tab1: React.FC = () => {
+  const imagePull = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +15,36 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonRow>
+          <IonCol className="header">
+            <img src={logo} alt="ink" />
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol class="container">
+            dropzone
+            {/* <ngx-dropzone class="drop" (change)="onSelect($event)" accept="image/jpeg,image/jpg,image/png" [multiple]="false">
+            <ion-icon name="add"></ion-icon>
+            <NgxDropzonePreviewComponent *ngFor="let f of files" [removable]="true" (removed)="onRemove(f)" class="drop-prev">
+            <ngx-dropzone-label class="drop-prev-text">Archivo ({{ f.type }})</ngx-dropzone-label>
+          </NgxDropzonePreviewComponent>
+        </ngx-dropzone> */}
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol class="container">
+            <div className="images">
+              <div className="images-container">
+                <div className="one-image" >
+                  {imagePull.map((image, i) => {
+                    return <img src={require('../assets/images/' + image + '.jpg')} key={i} alt="InkImage" />
+                  }
+                  )}
+                </div>
+              </div>
+            </div>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
--- InkSurrection/src/pages/Tab3.tsx
+++ InkSurrection/src/pages/Tab3.tsx
@@ -1,5 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonRow,IonCol } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+
+
+
 import './Tab3.css';
 const data = {
   image: 'https://www.w3schools.com/w3images/sound.jpg',
@@ -40,15 +43,6 @@ const Tab3: React.FC = () => {
           <IonTitle>Tab 3</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-        <div id="root"></div>
-      </IonContent>
     </IonPage>
   );
 };
